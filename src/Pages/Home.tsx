2067dc881900b1c15c93cde79629484c77007af2
shiyas ps <shiyasofficial1@gmail.com>
2026-03-27 18:33:09 +0530

    Revert "fixed some bugs"

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,8 +10,7 @@ import ExploreLC from "./Components/ExploreLC/ExploreLC";
 import Team from "./Components/Team/Team";
 import Connect from "./Components/Connect/Connect";
 import Footer from "./Components/Footer/Footer";
-
-
+import Achievements from "./Components/Achievements/Achievements";
 
 function Home() {
   return (
@@ -20,6 +19,7 @@ function Home() {
       <Hero />
       <About />
       <Events />
+      <Achievements />
       <Gallery />
       <Statistics />
       <ExploreLC />
